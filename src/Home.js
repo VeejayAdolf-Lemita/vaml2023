cb395f1857b1caf6ef653184e2e51452f5dc83e6
VeejayAdolf-Lemita <102731011+VeejayAdolf-Lemita@users.noreply.github.com>
2023-02-21 23:26:16 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillFacebook,
+} from "react-icons/ai";
+
+function Home() {
+  return (
+    <div className="text-center p-5 "> 
+      <h2 className="text-3xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl ">
+        Veejay Adolf
+      </h2>
+      <h3 className="text-xl py-2 dark:text-white md:text-3xl">
+        Front-end Developer and UI/UX designer.
+      </h3>
+      <p className="text-sm py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+        Developer providing services for  building (and occasionally designing) exceptional digital experiences.
+      </p>
+      <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+        <AiFillGithub />
+        <AiFillLinkedin />
+        <AiFillFacebook/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
